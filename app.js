ccd8357c2e1b872828a124f76cb8f159267a9a7a
marwanrawshedh <marwansalem55@yahoo.com>
2021-06-24 13:28:32 +0300

    branchproblem1

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+    let sumn=a+b;
+    console.log('The sum of '+a+' and ' +b+ ' is ' +sumn+'.')
+    return[sumn,'The sum of '+a+' and ' +b+ ' is ' +sumn+'.']
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
-
+testSum(4, 7,);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -27,6 +28,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+
 
 }
 
